3ff40d6514c898393076085f016cbac0150a30bf
Haututu <35088807+haututu@users.noreply.github.com>
2018-04-23 14:36:14 +1200

    Added uiRender for category

--- suicideApp/app.R
+++ suicideApp/app.R
@@ -19,57 +19,61 @@ dat <- readRDS("dat.RDS")
 ui <- fluidPage(
   
   useShinyjs(),
+  # Application title
+  titlePanel("Suicide Stats Explorer"),
   
-   # Application title
-   titlePanel("Suicide Stats Explorer"),
-   
-   p("Suicide statistics are difficult to access online. 
-     This app allows you to 'slice 'n dice' these stats in one place. 
-     It is a very early development version so likely to change substantially."),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        radioButtons("viewSelect",
-                    "Select view mode",
-                    c("Time series", "Cross section")),
-        selectInput("measureSelect",
-                    "Select measure",
-                    levels(dat$measure)),
-        selectInput("yearSelect",
-                    "Select year",
-                    levels(dat$year)),
-        div(id = "banana",
-        selectInput("categorySelect",
-                    "Select category",
-                    levels(dat$category),
-                    multiple = TRUE)),
-        
-        textOutput("blah"),
-        
-        actionButton("email", "Contact author")
-         
-      ),
+  p("Suicide statistics are difficult to access online. 
+    This app allows you to 'slice 'n dice' these stats in one place. 
+    It is a very early development version so likely to change substantially."),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("viewSelect",
+                   "Select view mode",
+                   c("Time series", "Cross section")),
+      selectInput("measureSelect",
+                  "Select measure",
+                  levels(dat$measure)),
+      selectInput("yearSelect",
+                  "Select year",
+                  levels(dat$year)),
+      uiOutput("categorySelect"),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotlyOutput("distPlot")
+        
+      actionButton("email", "Contact author")
+      
+      ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotlyOutput("distPlot")
       )
-   )
-)
+    )
+  )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
   observe({
     if(input$viewSelect == "Time series") {
-      show("categorySelect") 
-      hide("yearSelect")
+      show("categorySelect", anim = TRUE, time = 1) 
+      hide("yearSelect", anim = TRUE)
     } else {
-      hide("categorySelect") 
-      show("yearSelect")
+      hide("categorySelect", anim = TRUE) 
+      show("yearSelect", anim = TRUE, time = 1)
     }
   })
+  
+  output$categorySelect <- renderUI({
+    selectInput("categorySelect",
+                "Select category",
+                levels(
+                  droplevels(
+                    filter(dat, measure == input$measureSelect)$category
+                    )
+                  ),
+                multiple = TRUE)
+    })
    
   output$distPlot <- renderPlotly({
      if (input$viewSelect == "Cross section") {
@@ -89,22 +93,24 @@ server <- function(input, output) {
            )
        ) 
      } else {
-       ggplot(filter(dat, 
-                     measure == input$measureSelect & 
-                       category %in% input$categorySelect &
-                       category != "Off Shore"), 
-              aes(x=year, y=rate, group=category, color=category)) +
-         geom_point() +
-         geom_line() +
-         labs(
-           y = "Rate per 10,000"
-         ) +
-         theme_classic() +
-         theme(
-           axis.text.x = element_text(angle = 45, hjust = 1)
+       ggplotly(
+         ggplot(filter(dat, 
+                       measure == input$measureSelect & 
+                         category %in% input$categorySelect &
+                         category != "Off Shore"), 
+                aes(x=year, y=rate, group=category, color=category)) +
+           geom_point() +
+           geom_line() +
+           labs(
+             y = "Rate per 10,000"
+             ) +
+           theme_classic() +
+           theme(
+             axis.text.x = element_text(angle = 45, hjust = 1)
+           )
          )
-     }
-   })
+       }
+    })
    
 }
 
